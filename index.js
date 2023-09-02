40b451a8cb2e2495a797d75b7771e7d7908cf61f
AleksandrL0579 <a.lelsan@ukr.net>
2023-09-02 18:55:11 +0300

    jsTrain19

--- index.js
+++ index.js
@@ -19,6 +19,24 @@
 // Створюємо об'єкт Book
 
 console.log("Завдання: 1 ==============================");
+function Book() {
+  this.title = "Загальна Книга";
+  this.author = "Анонім";
+  this.pages = 0;
+}
+Book.prototype.read = function () {
+  console.log(`Ви читаєте ${this.title} від ${this.author}`);
+};
+Book.prototype.toString = function () {
+  return `{ title: '${this.title}', author: '${this.author}', pages: ${this.pages}, read: [Function (read)] }`;
+};
+const myBook = new Book();
+console.log("Об'єкт: Book", myBook.toString());
+console.log(
+  myBook.toString() ===
+    `{ title: 'Загальна Книга', author: 'Анонім', pages: 0, read: [Function (read)] }`
+);
+myBook.read();
 
 // Виводимо в консоль Об'єкт: Book
 
@@ -42,7 +60,23 @@ console.log("Завдання: 1 ==============================");
 // Додаємо властивість genre
 
 console.log("Завдання: 2 ==============================");
-
+function Book() {
+  this.title = "Загальна Книга";
+  this.author = "Анонім";
+  this.pages = 0;
+}
+Book.prototype.read = function () {
+  console.log(`Ви читаєте ${this.title} від ${this.author}`);
+};
+function Novel() {
+  Book.call(this);
+  this.genre = "Новела";
+}
+Novel.prototype = Object.create(Book.prototype);
+Novel.prototype.constructor = Novel;
+const myNovel = new Novel();
+console.log("Об'єкт: Novel", myNovel);
+console.log("Прототип об'єкту: Novel", Object.getPrototypeOf(myNovel));
 // Виводимо в консоль Об'єкт: Novel
 
 // Виводимо в консоль прототип Об'єкту: Novel
@@ -65,6 +99,29 @@ console.log("Завдання: 2 ==============================");
 // Змінемо прототип об'єкта Biography на Novel
 
 console.log("Завдання: 3 ==============================");
+function Book() {
+  this.title = "Загальна Книга";
+  this.author = "Анонім";
+  this.pages = 0;
+}
+Book.prototype.read = function () {
+  console.log(`Ви читаєте ${this.title} від ${this.author}`);
+};
+function Novel() {
+  Book.call(this);
+  this.genre = "Новела";
+}
+Novel.prototype = Object.create(Book.prototype);
+Novel.prototype.constructor = Novel;
+function Biography() {
+  this.title = "Загальна Біографія";
+  this.author = "Біограф";
+  this.pages = 200;
+}
+Biography.prototype = new Novel();
+const myBiography = new Biography();
+console.log(myBiography);
+console.log(myBiography instanceof Novel);
 // Виводимо в консоль Об'єкт: Biography
 
 // Перевіримо чи являється Novel прототипом Biography та виведемо в консоль
@@ -95,6 +152,40 @@ console.log("Завдання: 3 ==============================");
 // | info        | написана в 1915 році |
 
 console.log("Завдання: 4 ==============================");
+function Book() {
+  this.title = "Загальна Книга";
+  this.author = "Анонім";
+  this.pages = 0;
+}
+Book.prototype.read = function () {
+  console.log(`Ви читаєте ${this.title} від ${this.author}`);
+};
+function ScienceBook() {
+  Book.call(this);
+  let info = "написана в 1915 році";
+  Object.defineProperty(this, "info", {
+    get: function () {
+      return `Про книгу ${this.title}: ${info}`;
+    },
+    set: function (newInfo) {
+      info = newInfo;
+    },
+    enumerable: false,
+    configurable: false,
+  });
+  this.setInfo = function (newInfo) {
+    info = newInfo;
+  };
+}
+ScienceBook.prototype = Object.create(Book.prototype);
+ScienceBook.prototype.constructor = ScienceBook;
+const myScienceBook = new ScienceBook();
+myScienceBook.setInfo("написана в 1920 році");
+console.log("Властивість info:", myScienceBook.info);
+console.log(
+  "Налаштування властивості info:",
+  Object.getOwnPropertyDescriptor(myScienceBook, "info")
+);
 // Виводимо в консоль властивість info
 
 // Виводимо в консоль налаштування властивости info
@@ -118,6 +209,46 @@ console.log("Завдання: 4 ==============================");
 // | author      | "Дж. Д. Джонс"             |
 
 console.log("Завдання: 5 ==============================");
+function Book() {
+  this.title = "Загальна Книга";
+  this.author = "Анонім";
+  this.pages = 0;
+}
+Book.prototype.read = function () {
+  console.log(`Ви читаєте ${this.title} від ${this.author}`);
+};
+function ScienceBook() {
+  Book.call(this);
+  let info = "написана в 1915 році";
+
+  Object.defineProperty(this, "info", {
+    get: function () {
+      return `Про книгу ${this.title}: ${info}`;
+    },
+    enumerable: true,
+    configurable: false,
+  });
+
+  this.setInfo = function (newInfo) {
+    info = newInfo;
+  };
+}
+ScienceBook.prototype = Object.create(Book.prototype);
+ScienceBook.prototype.constructor = ScienceBook;
+function Textbook() {
+  ScienceBook.call(this);
+  this.read = function () {
+    console.log(
+      `Ви читаєте підручник "${this.title}" від ${this.author}. ${this.info}`
+    );
+  };
+}
+Textbook.prototype = Object.create(ScienceBook.prototype);
+Textbook.prototype.constructor = Textbook;
+const myTextbook = new Textbook();
+myTextbook.title = "Фізика у Вищій Школі";
+myTextbook.author = "Дж. Д. Джонс";
+myTextbook.read();
 // Викликаємо функцію read об'єкту Textbook
 
 // 6. Абстракція: створення об'єкта з загальними властивостями
@@ -154,4 +285,24 @@ console.log("Завдання: 5 ==============================");
 // | title       | "Загальна Пісня"       |
 
 console.log("Завдання: 6 ==============================");
+function Media() {
+  this.format = "Загальний Формат";
+  this.length = 0;
+}
+Media.prototype.play = function () {
+  console.log(
+    `Зараз відтворюється медіа у форматі ${this.format} з тривалістю ${this.length} секунд`
+  );
+};
+function Song() {
+  Media.call(this);
+  this.artist = "Загальний Виконавець";
+  this.title = "Загальна Пісня";
+}
+Song.prototype = Object.create(Media.prototype);
+Song.prototype.constructor = Song;
+const mySong = new Song();
+mySong.artist = "Виконавець";
+mySong.title = "Пісня";
+mySong.play();
 // Викликаємо функцію play об'єкту Song
